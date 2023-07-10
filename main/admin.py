c2a293ef6eebcf5dea1d470de4aa478a64b22b6b
RafidIncrementsInc <120081404+RafidIncrementsInc@users.noreply.github.com>
2023-07-11 02:39:13 +0600

    login done, post try

--- main/admin.py
+++ main/admin.py
@@ -8,9 +8,25 @@ from main import models
 class UserAdmin(admin.ModelAdmin):
     pass
 
+
 class PostAdmin(admin.ModelAdmin):
     pass
 
-admin.site.register(models.User, UserAdmin)
 
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+class BanglaCategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+class EnglishCategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Tag, TagAdmin)
+admin.site.register(models.BanglaCategory, BanglaCategoryAdmin)
+admin.site.register(models.EnglishCategory, EnglishCategoryAdmin)
 admin.site.register(models.Post, PostAdmin)
